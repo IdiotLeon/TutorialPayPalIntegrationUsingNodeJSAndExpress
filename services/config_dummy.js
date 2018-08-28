f0efbaec532bdcf6653e582f95ad77e1ad9c3a72
Leon Lu <yanglu@microsoft.com>
2018-08-27 20:59:21 -0700

    demo: Creating the Configuration

---
+++ services/config_dummy.js
@@ -0,0 +1,15 @@
+((configRepo) => {
+    configRepo.SetConfig = (paypal) => {
+        console.log("Config Hit");
+        var config = {
+            "host": "api.sandbox.paypal.com",
+            "port": "",
+            "client_id": "",
+            "client_secret": ""
+        }
+    }
+    paypal.configure(config);
+})
+    (
+    module.exports
+    )
